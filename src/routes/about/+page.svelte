0a5e74d08e5b02f06fd6a84de76ef1e19a2a155f
Chandram-Dutta <chandramdutta2004@gmail.com>
2024-02-01 23:58:41 +0530

    feat: added table creation code

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,28 @@
+<h1 class="text-primary text-xl font-bold">
+    Triton - GDSC VIT's pastebin and URL shortener
+</h1>
+Sharing short code samples, logs or links is now easier than ever!
+
+<h1 class="text-primary pt-8 text-xl font-bold">Basics</h1>
+The easiest way to use triton, is from inside your browser. Type or paste your content
+into the text area on the homepage, save it, and copy the link. You can now share
+this link with whoever you want.If you want to create a short url from a link, simply
+paste the link in the text field. You can now copy the resulting link and it will
+automatically redirect everyone.
+
+<h1 class="text-primary pt-8 text-xl font-bold">Editing</h1>
+You can edit your documents by clicking the edit button When you create your first
+document we will automatically create an (anonymous) account for you, so you will
+be able to edit your documents for as long as you keep our cookie.
+
+<h1 class="text-primary pt-8 text-xl font-bold">Document lifetime</h1>
+There is no explicit expiration time for documents on triton. Documents may however
+be removed at any time without notice.
+
+<h1 class="text-primary pt-8 text-xl font-bold">Open Source</h1>
+Triton is fully open source and built on open source technology. All sources can
+be found on GitHub. You can easily deploy it yourself wherever you want.
+
+<h1 class="text-primary pt-8 text-xl font-bold">Technology</h1>
+Triton is built on top of SvelteKit, TailwindCSS, DaisyUI, Drizzle ORM and Postgres.
+It is hosted on Vercel.
---
+++ src/routes/api/create-cope-table/+server.ts
@@ -0,0 +1,17 @@
+import { sql } from "@vercel/postgres";
+
+export async function GET({}) {
+  try {
+    const result = await sql`CREATE TABLE IF NOT EXISTS triton (
+            file_name text PRIMARY KEY NOT NULL,
+            file_data text NOT NULL,
+            created_at timestamp NOT NULL default current_timestamp,
+            updated_at timestamp NOT NULL default current_timestamp,
+            type text NOT NULL,
+            ownerID uuid NOT NULL
+        );`;
+    return Response.json({ result });
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
